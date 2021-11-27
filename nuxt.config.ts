94466c0d8e12759fb6268daa31ce7dc415f6b2bd
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-28 01:21:21 +0800

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,24 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
-  title: 'Vitesse Nuxt 3',
+  meta: {
+    title: 'Vitesse Nuxt 3',
+  },
   buildModules: [
     '@vueuse/core/nuxt',
     '@unocss/nuxt',
     '@pinia/nuxt',
   ],
   unocss: {
-    shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ],
     uno: true,
     attributify: true,
     preflight: true,
     icons: {
       scale: 1.2,
     },
+    shortcuts: [
+      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ],
   },
   vite: {
     logLevel: 'info',
