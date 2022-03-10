a09387324399d7add7e84e2f6fc69ee0e58f4b0a
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-10 20:39:09 +0800

    chore: update

--- components/Counter.vue
+++ components/Counter.vue
@@ -5,13 +5,13 @@ const { count, inc, dec } = useCount()
 <template>
   <div inline-flex m="y-3">
     <button btn p-2 rounded-full @click="dec()">
-      <div i-carbon-subtract></div>
+      <div i-carbon-subtract />
     </button>
     <div font="mono" w="15" m-auto inline-block>
       {{ count }}
     </div>
     <button btn p-2 rounded-full @click="inc()">
-      <div i-carbon-add></div>
+      <div i-carbon-add />
     </button>
   </div>
 </template>
