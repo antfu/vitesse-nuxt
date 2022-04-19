581b36d3aa11e1c6adc4924954d916661dc850e0
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-19 10:55:43 +0800

    feat: enable `reactivityTransform`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,8 @@ export default defineNuxtConfig({
       ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ],
   },
-  // experimental: {
-  //   viteNode: true,
-  // },
+  experimental: {
+    reactivityTransform: true,
+    // viteNode: true,
+  },
 })
