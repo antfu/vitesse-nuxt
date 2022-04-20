bc91b8784efbf0352c2bffbad3499b7195a86377
LoTwT <709937065@qq.com>
2022-04-20 23:11:52 +0800

    fix: <a> target

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,7 +7,7 @@
     gap-3
   >
     <NuxtLink i-carbon-campsite to="/" />
-    <a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="__blank" />
+    <a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="_blank" />
     <DarkToggle />
   </div>
 </template>
