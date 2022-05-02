ffb8725ac14fcf2e4516d1911cc22d195c3a9f44
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-03 05:09:07 +0800

    chore: update deps

--- components/InputEntry.vue
+++ components/InputEntry.vue
@@ -14,20 +14,16 @@ const go = () => {
       id="input"
       v-model="name"
       placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      m="t-5"
-      w="250px"
-      text="center"
-      bg="transparent"
+      type="text" autocomplete="off"
+      p="x-4 y-2" m="t-5" w="250px"
+      text="center" bg="transparent"
       border="~ rounded gray-200 dark:gray-700"
       outline="none active:none"
       @keydown.enter="go"
     >
     <div>
       <button
-        class="m-3 text-sm btn"
+        m-3 text-sm btn
         :disabled="!name"
         @click="go"
       >
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,21 +9,10 @@ export default defineNuxtConfig({
   ],
   experimental: {
     reactivityTransform: true,
-    // viteNode: true,
-  },
-  vueuse: {
-    ssrHandlers: true,
+    viteNode: true,
   },
   unocss: {
-    uno: true,
-    attributify: true,
     preflight: true,
-    icons: {
-      scale: 1.2,
-    },
-    shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ],
   },
   colorMode: {
     classSuffix: '',
