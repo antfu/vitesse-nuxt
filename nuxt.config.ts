59c1a2bc05c7e7921d2e70883921ba605cccea46
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-05-19 16:32:40 +0800

    feat: add nuxt-lodash module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
     '@unocss/nuxt',
     '@pinia/nuxt',
     '@nuxtjs/color-mode',
+    'nuxt-lodash',
   ],
   build: {
     // https://github.com/element-plus/element-plus-nuxt-starter/blob/44644788ee0d2a2580769769f9885b5cd9f7c0ab/nuxt.config.ts#L27
@@ -29,11 +30,17 @@ export default defineNuxtConfig({
     strict: true,
     typeCheck: true,
   },
+  // https://github.com/victorgarciaesgi/nuxt-typed-router
   nuxtTypedRouter: {
     // Output directory where you cant the files to be saved
     // outDir: './generated',
     // Name of the routesNames object
     // routesObjectName: 'routerPagesNames',
+  },
+  // https://github.com/cipami/nuxt-lodash
+  lodash: {
+    prefix: 'lo',
+    prefixSkip: [],
   },
   css: ['~/styles/index.scss'],
   unocss: {
