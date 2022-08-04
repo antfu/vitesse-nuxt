67428fdc3728582f1303dd58b8e233d1d5619d96
Anthony Fu <anthonyfu117@hotmail.com>
2022-08-04 23:09:48 +0800

    fix: netlify workaround

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,4 +17,14 @@ export default defineNuxtConfig({
   colorMode: {
     classSuffix: '',
   },
+  // https://github.com/nuxt/framework/issues/6204#issuecomment-1201398080
+  hooks: {
+    'vite:extendConfig': function (config: any, { isServer }: any) {
+      if (isServer) {
+        // Workaround for netlify issue
+        // https://github.com/nuxt/framework/issues/6204
+        config.build.rollupOptions.output.inlineDynamicImports = true
+      }
+    },
+  },
 })
