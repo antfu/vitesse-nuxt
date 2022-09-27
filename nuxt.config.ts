0467bd915a32333354f1129209ed45d7b2249f72
Anthony Fu <anthonyfu117@hotmail.com>
2022-09-27 09:38:26 +0800

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   modules: [
@@ -9,22 +9,11 @@ export default defineNuxtConfig({
   ],
   experimental: {
     reactivityTransform: true,
-    viteNode: false,
   },
   unocss: {
     preflight: true,
   },
   colorMode: {
     classSuffix: '',
-  },
-  // https://github.com/nuxt/framework/issues/6204#issuecomment-1201398080
-  hooks: {
-    'vite:extendConfig': function (config: any, { isServer }: any) {
-      if (isServer) {
-        // Workaround for netlify issue
-        // https://github.com/nuxt/framework/issues/6204
-        config.build.rollupOptions.output.inlineDynamicImports = true
-      }
-    },
   },
 })
