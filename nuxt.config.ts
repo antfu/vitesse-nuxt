9ef5d4b1d15180875d42e69c84f44f43e83bf859
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-12 11:43:34 +0200

    fix: enable `renderJsonPayloads`, close #84

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
     // but missing on offline, disabling extraction it until fixed
     payloadExtraction: false,
     inlineSSRStyles: false,
+    renderJsonPayloads: true,
   },
 
   css: [
