1c429961e4f94979081474f5fd18c62c712cc8ed
Anthony Fu <anthonyfu117@hotmail.com>
2024-03-28 10:35:12 +0100

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import antfu from '@antfu/eslint-config'
 import nuxt from './.nuxt/eslint.config.mjs'
 
 export default nuxt(
-  // @ts-expect-error types are not compatible yet
   antfu(
     {
       unocss: true,
