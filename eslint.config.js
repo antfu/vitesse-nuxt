cb26a3a2c83215a10caa0a57a908528d6619ff37
Anthony Fu <github@antfu.me>
2025-03-25 16:11:17 +0900

    chore: migrate to pnpm catalogs

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,11 @@
 import antfu from '@antfu/eslint-config'
 import nuxt from './.nuxt/eslint.config.mjs'
 
-export default nuxt(
-  antfu(
-    {
-      unocss: true,
-      formatters: true,
-    },
-  ),
+export default antfu(
+  {
+    unocss: true,
+    formatters: true,
+    pnpm: true,
+  },
 )
+  .append(nuxt())
